f14c315a00252d7e26877b65b539ef8837c866c8
Giles Dring <giles@dringtech.com>
2024-02-20 18:07:25 +0000

    Bundle library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/charts.js'),
+      name: 'ifg-charts',
+      fileName: (format) => `ifg-charts.${format}.js`
+    }
+  }
 })
