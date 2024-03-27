5207c50f10c0697cdfa55c327198c29ff8cf3cce
Giles Dring <giles@dringtech.com>
2024-03-27 16:14:55 +0000

    Update category colours

--- src/lib/categoryColours.js
+++ src/lib/categoryColours.js
@@ -1,102 +1,102 @@
-export const categoryColours = {
-  'gender-m': {
-    colour: '#00c7b1',
-    label: 'Male',
-  },
+export const categoryColours = { 
   'gender-f': {
-    colour: '#84329b',
-    label: 'Female',
+      colour: '#84329b',
+      label: 'Female'
+  },
+  'gender-m': {
+      colour: '#00c7b1',
+      label: 'Male'
   },
   'gender-x': {
-    colour: '#ED8B00',
-    label: 'Other',
+      colour: '#ED8B00',
+      label: 'Other'
   },
   'rank-equivalence-pm': {
-    colour: '#00C7B1',
-    label: 'Prime minister',
+      colour: '#00C7B1',
+      label: 'Prime minister'
   },
   'rank-equivalence-dpm': {
-    colour: '#44FFEC',
-    label: 'Deputy prime minister',
+      colour: '#44FFEC',
+      label: 'Deputy prime minister'
   },
   'rank-equivalence-sos': {
-    colour: '#D0006F',
-    label: 'Secretary of state',
+      colour: '#D0006F',
+      label: 'Secretary of state'
   },
   'rank-equivalence-mos': {
-    colour: '#00A8E1',
-    label: 'Minister of state',
+      colour: '#00A8E1',
+      label: 'Minister of state'
   },
   'rank-equivalence-puss': {
-    colour: '#54D4FF',
-    label: 'Parliamentary under secretary of state',
-  },
-  'rank-equivalence-on-leave': {
-    colour: '#c1c5c8',
-    label: 'On leave',
+      colour: '#54D4FF',
+      label: 'Parliamentary under secretary of state'
   },
   'rank-equivalence-parl-lead': {
-    colour: '#622574',
-    label: 'Parliamentary leadership',
+      colour: '#622574',
+      label: 'Parliamentary leadership'
   },
   'rank-equivalence-parl': {
-    colour: '#BD73D2',
-    label: 'Other parliamentary role',
+      colour: '#BD73D2',
+      label: 'Other parliamentary role'
   },
-  'party-conservative': {
-    colour: '#00539f',
-    label: 'Conservative',
-  },
-  'party-labour': {
-    colour: '#ee3224',
-    label: 'Labour',
-  },
-  'party-liberal-democrat': {
-    colour: '#ffb703',
-    label: 'Liberal Democrat',
-  },
-  'party-snp': {
-    colour: '#fff95d',
-    label: 'SNP',
-  },
-  'party-other': {
-    colour: '#c1c5c8',
-    label: 'Independent/Other',
-  },
-  'party-plaid-cymru': {
-    colour: '#3f8429',
-    label: 'Plaid Cymru',
-  },
-  'party-green': {
-    colour: '#6ab023',
-    label: 'Green',
-  },
-  'party-ukip': {
-    colour: '#722889',
-    label: 'UKIP',
-  },
-  'party-reform': {
-    colour: '#3bb7ce',
-    label: 'Reform UK',
-  },
-  'party-dup': {
-    colour: '#8f1d20',
-    label: 'DUP',
-  },
-  'party-sinn-fein': {
-    colour: '#006837',
-    label: 'Sinn Féin',
+  'rank-equivalence-on-leave': {
+      colour: '#c1c5c8',
+      label: 'On leave'
   },
   'party-alliance': {
-    colour: '#f2d303',
-    label: 'Alliance',
+      colour: '#f2d303',
+      label: 'Alliance'    
+  },
+  'party-conservative': {
+      colour: '#00539f',
+      label: 'Conservative'
+  },
+  'party-dup': {
+      colour: '#8f1d20',
+      label: 'DUP'
+  },
+  'party-green': {
+      colour: '#6ab023',
+      label: 'Green'
+  },
+  'party-labour': {
+      colour: '#ee3224',
+      label: 'Labour'
+  },
+  'party-liberal-democrat': {
+      colour: '#ffb703',
+      label: 'Liberal Democrat'
+  },
+  'party-plaid-cymru': {
+      colour: '#3f8429',
+      label: 'Plaid Cymru'
+  },
+  'party-reform': { 
+      colour: '#3bb7ce',
+      label: 'Reform UK'
   },
   'party-sdlp': {
-    colour: '#0e6e51',
-    label: 'SDLP',
+      colour: '#0e6e51',
+      label: 'SDLP'
+  },
+  'party-sinn-fein': {
+      colour: '#006837',
+      label: 'Sinn Féin'
+  },
+  'party-snp': {
+      colour: '#fff95d',
+      label: 'SNP'
+  },
+  'party-ukip': {
+      colour: '#722889',
+      label: 'UKIP'
   },
   'party-uup': {
-    colour: '#0eaefe',
-    label: 'UUP',
+      colour: '#0eaefe',
+      label: 'UUP'
+  },
+  'party-other': {
+      colour: '#c1c5c8',
+      label: 'Independent/Other'    
   },
 };
