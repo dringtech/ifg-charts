3103a589e47303e6c3b9bee18e8163e5f45c6c72
Giles Dring <giles@dringtech.com>
2024-06-13 11:22:30 +0100

    Add example to live site

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 import { timeline } from './dist/ifg-charts.es.js';
-import { documentationExample } from './test.js';
+import { documentationExample, options } from './test.js';
+
 timeline("timeline-chart", documentationExample);
+timeline("timeline-chart-full", options);
 
 const docExample = document.getElementById('documented-example');
 docExample.innerHTML = JSON.stringify(documentationExample, null, 2);
